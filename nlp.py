b1ef36d7ac654fc3d591f1026540c5044995c15d
Saravjot Singh <saravjot.singh@grazitti.com>
2021-08-06 00:16:48 +0530

    added pickle file to save model

--- nlp.py
+++ nlp.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer
 import scipy
+import pickle
 
 model = SentenceTransformer('bert-base-nli-mean-tokens')
 
@@ -7,16 +8,28 @@ sentences = ['Where is the wedding', 'how old are you', 'i want to book a resort
 
 sentence_embeddings = model.encode(sentences)
 
-queries  = ['I need a break']
+# Saving our sentence into model file
+with open('embedings.pkl',"wb") as fOut:
+    pickle.dump({'sentences':sentences, 'embedings' : sentence_embeddings},fOut)
 
+
+# Query to search with matching sentences
+queries  = ['I need a break']
 queries_embedding = model.encode(queries)
 
+
+# Load the model saved earlier
+with open('embedings.pkl','rb') as fIn:
+    stored_data = pickle.load(fIn)
+    stored_sentences = stored_data['sentences']
+    stored_embedings = stored_data['embedings']
+
 for query, query_embedding in zip(queries,queries_embedding):
-    cosine_distance = scipy.spatial.distance.cdist([query_embedding],sentence_embeddings,"cosine")[0]
+    cosine_distance = scipy.spatial.distance.cdist([query_embedding],stored_embedings,"cosine")[0]
     results = zip(range(len(cosine_distance)),cosine_distance)
     results = sorted(results, key=lambda x:x[1])
 
     print("Top sentence according to Query => ", query)
 
     for idx, distance in results[0:5]:
-        print(sentences[idx] , " consine similarity " ,1 - distance)
+        print(stored_sentences[idx] , " consine similarity " ,1 - distance)
